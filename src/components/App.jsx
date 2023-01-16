9b6521fe54ac1a9140ea78c03a6851b20598612d
Qui Van <quivan3396@yahoo.com>
2023-01-16 10:03:31 -0500

    remove component to test build fail for netlify

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PictureCard from "./PictureCard";
 import Navbar from "./Navbar";
 import black from "../images/logos/transparent_no_shade.png";
-import Icon from "./Icon";
 
 function App() {
   return (
