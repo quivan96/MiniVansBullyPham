d2b3e730742bf70a545e906d1e6a47304bfea974
Qui Van <quivan3396@yahoo.com>
2023-03-12 00:36:55 -0500

    add footer import

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import style from "./footer.module.css";
 
 const Footer = (props) => {
   return (
     <div className="row">
       <div className="col-12">
-        <footer className="row">
+        <footer className={`${style.footer} row`}>
           <div className="col-6 mx-auto">{props.children}</div>
         </footer>
       </div>
